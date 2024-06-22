b4f6bcc9d8c231502a127e0e65b9afa96a8e5b4a
vladisla5 <103644908+vladisla5@users.noreply.github.com>
2024-06-22 13:27:13 +0300

    number 1

--- myproj/urls.py
+++ myproj/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import auth, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('log_in/', auth),
+    path('index/', index)
 ]
