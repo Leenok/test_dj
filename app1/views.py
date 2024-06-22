9b47bb4cc53f94371840eeea8359be242bd2d279
Lenok <eseredich8@gmail.com>
2024-06-22 13:25:55 +0300

    feat: add log in and log uot

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def reg(request):
+    # res = User.objects.all()
+    if request.method == "POST":
+        not_valid = ''
+        data = request.POST
+        user = User.objects.create_user(data['login'],data['email'],data['password'])
+        user.first_name = data['name']
+
+        if data['password'] == data['password2']:
+            user.save()
+            auth(request)
+            return redirect(index)
+        
+        else:
+            not_valid = 'Неверный пароль'
+            return render(request, 'reg.html',  {'not_valid_regist': not_valid})
+        
+    else:
+        return render(request, 'reg.html')
+    
+
+def auth(request):
+    if request.method == "POST":
+        data = request.POST
+        user = authenticate(username = data['login'], password = data['password'])
+        if user is not None:
+            request.session['is_auth'] = user.username
+            return redirect(index)
+        else:
+            return render(request, 'auth.html')
+    else:
+        return render(request, 'auth.html')
--- myproj/urls.py
+++ myproj/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import index, auth, reg
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index),
+    path('auth/', auth),
+    path('reg/', reg),
 ]
